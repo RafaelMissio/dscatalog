7eb6cc95e05ddb128bc3b4dfb08d344afdd9c4fe
rafaelmissio <rtmissio@gmail.com>
2025-07-26 05:16:14 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/missio/dscataslog/DscataslogApplication.java
@@ -0,0 +1,13 @@
+package br.com.missio.dscataslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscataslogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscataslogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/missio/dscataslog/DscataslogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.missio.dscataslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscataslogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
